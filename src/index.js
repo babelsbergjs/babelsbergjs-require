c37d63f29328d93c48247589926f7b01b7c4a7f0
FHorschig <f.horschig@googlemail.com>
2015-07-01 19:32:54 +0200

    Readjusted module path. New Release.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ var BabelsbergSrcTransform = require('./src_transform.js')
 var fs = require('fs');
 
 module.exports = function bbb_require(filename) {
-  var Module = module.constructor;
-  var m = new Module();
   var src = fs.readFileSync(filename, 'utf8');
   src = new BabelsbergSrcTransform().transform(src);
-  m._compile(src, filename);
-  return m.exports;
+  module._compile(src, filename);
+  return module.exports;
 }
