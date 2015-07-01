6807912444724af0847e49a81751224251131162
FHorschig <f.horschig@googlemail.com>
2015-07-01 13:39:37 +0200

    First successful tranformation. (acc. to tests)

---
+++ src/index.js
@@ -0,0 +1,7 @@
+var BabelsbergSrcTransform = require('./src_transform.js')
+
+module.exports = function bbb_require(src) {
+  // 1. Load file
+  // 2. Transform contents
+  // 3. require Transformation
+}
